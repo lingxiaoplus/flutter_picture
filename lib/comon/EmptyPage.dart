6cc58d3ed4c5a79ea485737adf20ebd72b90694f
lingxiaopua <1083514716@qq.com>
2019-07-29 18:11:36 +0800

    专辑详情页面完成，添加空布局和网络错误布局

--- lib/comon/EmptyPage.dart
+++ lib/comon/EmptyPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 class EmptyPage extends StatefulWidget  {
   @override
   State<StatefulWidget> createState() {
@@ -16,9 +16,19 @@ class EmptyPageState extends State<EmptyPage> with SingleTickerProviderStateMixi
   void initState() {
     super.initState();
     controller = new AnimationController(
-        duration: const Duration(seconds: 1), vsync: this);
+        duration: const Duration(seconds: 2), vsync: this);
     //图片宽高从0变到300
-    animation = new Tween(begin: 0.0, end: 80.0).animate(controller);
+    animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+    animation = new Tween(begin: 0.0, end: math.pi * 2).animate(animation);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        //动画执行结束时反向执行动画
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        //动画恢复到初始状态时执行动画（正向）
+        controller.forward();
+      }
+    });
     //启动动画
     controller.forward();
   }
@@ -45,9 +55,9 @@ class AnimatedImage extends AnimatedWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Icon(Icons.hourglass_empty,
-              size: animation.value,
-            ),
+            Transform.rotate(angle: animation.value,child: Icon(Icons.hourglass_empty,
+              size: 80.0,
+            ),),
             Text('空空如也!!')
           ],
         ),
